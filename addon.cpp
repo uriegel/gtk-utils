dad0a337fe7739251638b3b055d94966fff8ba02
uriegel <uriegel@hotmail.de>
2020-01-02 22:38:50 +0100

    getSetting

--- addon.cpp
+++ addon.cpp
@@ -5,37 +5,49 @@ using namespace std;
 
 // TODO: sudo apt-get install libglib2.0-dev
 
-auto v8_value_to_utf8_string(const string& str) {
-	auto utf8_string = static_cast<char*>(g_malloc(str.length() + 1));
-	strcpy(utf8_string, str.c_str());
-	return utf8_string;
+auto v8_string_to_gtk_string(const string& str) {
+	auto gtk_string = static_cast<char*>(g_malloc(str.length() + 1));
+	strcpy(gtk_string, str.c_str());
+	return gtk_string;
 }
 
-Value Test(const CallbackInfo& info) {
-    auto schema_id = v8_value_to_utf8_string(info[0].As<String>().Utf8Value());
-    auto key = v8_value_to_utf8_string(info[1].As<String>().Utf8Value());
+Value get_setting(const CallbackInfo& info) {
+    auto schema_id = v8_string_to_gtk_string(info[0].As<String>().Utf8Value());
+    auto key = v8_string_to_gtk_string(info[1].As<String>().Utf8Value());
 
     auto schema_source = g_settings_schema_source_get_default();
+    if (!schema_source) {
+        g_free(static_cast<void*>(schema_id));
+        g_free(static_cast<void*>(key));
+        throw Napi::Error::New(info.Env(), "No schema");
+    }
+    
     auto schema = g_settings_schema_source_lookup (schema_source, schema_id, FALSE);
+    if (!schema) {
+        g_free(static_cast<void*>(schema_id));
+        g_free(static_cast<void*>(key));
+        throw Napi::Error::New(info.Env(), "No schema: "s + schema_id);
+    }
 
     auto gsettings_key = g_settings_schema_get_key(schema, key);
+    if (!gsettings_key) {
+        g_free(static_cast<void*>(schema_id));
+        g_free(static_cast<void*>(key));
+        throw Napi::Error::New(info.Env(), "Invalid key: "s + key);
+    }
 
     auto settings = g_settings_new(schema_id);
     auto variant = g_settings_get_value(settings, key);
 
-    auto is_da = schema != nullptr;
     g_free(static_cast<void*>(schema_id));
     g_free(static_cast<void*>(key));
-
+    
     auto erg = g_variant_get_string(variant, nullptr);
-
-    auto result = String::New(info.Env(), erg);
-
-    return result;
+    return String::New(info.Env(), erg);
 }
 
 Object Init(Env env, Object exports) {
-    exports.Set(String::New(env, "test"), Function::New(env, Test));
+    exports.Set(String::New(env, "getSetting"), Function::New(env, get_setting));
     return exports;
 }
 
--- dev/index.js
+++ dev/index.js
@@ -1,13 +1,12 @@
 const addon = require('../build/Debug/gtk-utils')
 
-const rett = addon.test("org.gnome.desktop.interface", "gtk-theme")
+const test = addon.getSetting("org.gnome.desktop.interface", "gtk-theme")
 let start = process.hrtime.bigint()
 
-for  (let i = 0; i < 1; i++) {
-    const rett = addon.test("org.gnome.desktop.interface", "gtk-theme")
+let result
+for  (let i = 0; i < 1000; i++) {
+    result = addon.getSetting("org.gnome.desktop.interface", "gtk-theme")
 }
 let end = process.hrtime.bigint()
-console.info(`Execution time getExifDate (hr): ${((end - start) / BigInt(1000000.0))} ms`)
-
-
-console.log("org.gnome.desktop.interface")
+console.info(`Execution time: ${((end - start) / BigInt(1000000.0))} ms`)
+console.log(result)
